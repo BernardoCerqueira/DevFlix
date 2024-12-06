847f302133ef18dfb5a6008ef9bf7b3ebce11227
Bernardo Cerqueira <katanardo@gmail.com>
2024-12-06 09:14:13 -0300

    Initialized prisma

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from "express"
+import {prisma} from "./database"
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    try {
+        await prisma.$connect()
+        console.log("DB connection successful.")
+    } catch (error) {
+        console.error("DB connection failed: ", error)
+    }
+
     console.log(`Server started successfully at port ${PORT}`)
 })
