6595c11f3a4abf7a25eaf21f6ab39a026fd139ea
Bernardo Cerqueira <katanardo@gmail.com>
2024-12-10 21:23:17 -0300

    File Upload (Episodes Model)

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,10 +1,21 @@
 import { ResourceWithOptions } from "adminjs";
-import { Category } from "../../models";
+import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
+import { courseResourceOptions } from "./course";
+import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Category,
     options: categoryResourceOptions
+  },
+  {
+    resource: Course,
+    options: courseResourceOptions
+  },
+  {
+    resource: Episode,
+    options: episodeResourceOptions,
+    features: episodeResourceFeatures
   }
 ]
