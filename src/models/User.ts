28ef51a2e790aff2be886486cabbcea0f3de2b13
Bernardo Cerqueira <katanardo@gmail.com>
2024-12-16 11:07:45 -0300

    erro

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,8 @@ import { sequelize } from '../database'
 import { DataTypes, Model, Optional } from 'sequelize'
 import bcrypt from 'bcrypt'
 
+type CheckPasswordCallback = (err?: Error, isSame?: boolean) => void
+
 export interface User {
     id: number
     firstName: string
@@ -15,7 +17,9 @@ export interface User {
 
 export interface UserCreationAttributes extends Optional<User, 'id'> { }
 
-export interface UserInstance extends Model<User, UserCreationAttributes>, User { }
+export interface UserInstance extends Model<User, UserCreationAttributes>, User {
+    checkPassword: (password: string, callbackfn: CheckPasswordCallback) => void
+}
 
 export const User = sequelize.define<UserInstance, User>('User', {
     id: {
@@ -62,9 +66,19 @@ export const User = sequelize.define<UserInstance, User>('User', {
 }, {
     hooks: {
         beforeSave: async (user) => {
-            if(user.isNewRecord || user.changed('password')) {
+            if (user.isNewRecord || user.changed('password')) {
                 user.password = await bcrypt.hash(user.password.toString(), 10)
             }
         }
     }
 })
+
+User.prototype.checkPassword = function (password: string, callbackfn: (err: Error | undefined, isSame: boolean) => void) {
+    bcrypt.compare(password, this.password, (err, isSame) => {
+        if (err) {
+            callbackfn(err, false)
+        } else {
+            callbackfn(err, isSame)
+        }
+    })
+}
