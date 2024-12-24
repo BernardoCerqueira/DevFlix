6f21b8876b25040611202a1dfd2f470e3c399762
Bernardo Cerqueira <katanardo@gmail.com>
2024-12-24 14:40:04 -0300

    Header HomeNoAuth responsivo

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,21 @@
 import Head from "next/head"
-import styles from "@/styles/HomeNoAuth.module.scss"
+import HeaderNoAuth from "@/components/homeNoAuth/headerNoAuth"
 
 const HomeNoAuth = () => {
   return(
     <>
       <Head>
-        <title>DevFlix</title>
+        <title>OneBitFlix</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
-        <meta property="og:title" content="DevFlix" key="title"/>
+        <meta property="og:title" content="OneBitFlix" key="title"/>
         <meta 
           name="description"
           content="Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil!"
         />
       </Head>
-      <main></main>
+      <main>
+        <HeaderNoAuth/>
+      </main>
     </>
   )
 }
