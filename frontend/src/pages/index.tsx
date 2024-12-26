f110e67abc02498dd3da87b554d41132ffc85831
Bernardo Cerqueira <katanardo@gmail.com>
2024-12-26 13:41:26 -0300

    Seção de apresentação

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head"
+import styles from "../styles/HomeNoAuth.module.scss"
 import HeaderNoAuth from "@/components/homeNoAuth/headerNoAuth"
+import PresentationSection from "@/components/homeNoAuth/presentationSection"
 
 const HomeNoAuth = () => {
   return(
@@ -14,7 +16,10 @@ const HomeNoAuth = () => {
         />
       </Head>
       <main>
-        <HeaderNoAuth/>
+        <div className={styles.sectionBackground}>
+          <HeaderNoAuth/>
+          <PresentationSection/>
+        </div>
       </main>
     </>
   )
