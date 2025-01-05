bec8780728ebdfb7a2c6a039875613e0faca0de3
Bernardo Cerqueira <katanardo@gmail.com>
2025-01-05 20:03:13 -0300

    Curso em destaque - Home

--- frontend/src/services/courseService.ts
+++ frontend/src/services/courseService.ts
@@ -1,3 +1,4 @@
+import { error } from "console"
 import api from "./api"
 
 export type EpisodeType = {
@@ -19,17 +20,34 @@ export type CourseType = {
 
 const courseService = {
     getNewestCourses: async (): Promise<CourseType[] | null> => {
-        try {
-          const res = await api.get("/courses/newest")
-          return res.data
-        } catch (error: any) {
-          console.error(
-            "Erro ao buscar os cursos mais recentes:",
-            error.response?.data?.message || error.message
-          )
-          return null
+      try {
+        const res = await api.get("/courses/newest")
+        return res.data
+      } catch (error: any) {
+        console.error(
+          "Erro ao buscar os cursos mais recentes:",
+          error.response?.data?.message || error.message
+        )
+        return null
+      }
+    },
+
+    getFeaturedCourses: async () => {
+      const token = sessionStorage.getItem("onebitflix-token")
+
+      const res = await api.get("/courses/featured", {
+        headers: {
+          Authorization: `Bearer ${token}`
         }
-      },
+      })
+        .catch((error) => {
+          console.log(error.response.data.message)
+
+          return error.response
+        })
+
+      return res
+    }
 }
 
 export default courseService
